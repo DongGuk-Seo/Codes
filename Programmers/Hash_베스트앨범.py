f2190832e4ea074bb1eed02b84af872242024cfd
DongGuk-Seo <failnxid@gmail.com>
2022-05-16 16:22:50 +0900

    new

---
+++ Programmers/Hash_베스트앨범.py
@@ -0,0 +1,59 @@
+# 문제 설명
+# 스트리밍 사이트에서 장르 별로 가장 많이 재생된 노래를 두 개씩 모아 베스트 앨범을 출시하려 합니다. 노래는 고유 번호로 구분하며, 노래를 수록하는 기준은 다음과 같습니다.
+
+# 속한 노래가 많이 재생된 장르를 먼저 수록합니다.
+# 장르 내에서 많이 재생된 노래를 먼저 수록합니다.
+# 장르 내에서 재생 횟수가 같은 노래 중에서는 고유 번호가 낮은 노래를 먼저 수록합니다.
+# 노래의 장르를 나타내는 문자열 배열 genres와 노래별 재생 횟수를 나타내는 정수 배열 plays가 주어질 때, 베스트 앨범에 들어갈 노래의 고유 번호를 순서대로 return 하도록 solution 함수를 완성하세요.
+
+# 제한사항
+# genres[i]는 고유번호가 i인 노래의 장르입니다.
+# plays[i]는 고유번호가 i인 노래가 재생된 횟수입니다.
+# genres와 plays의 길이는 같으며, 이는 1 이상 10,000 이하입니다.
+# 장르 종류는 100개 미만입니다.
+# 장르에 속한 곡이 하나라면, 하나의 곡만 선택합니다.
+# 모든 장르는 재생된 횟수가 다릅니다.
+# 입출력 예
+# genres	plays	return
+# ["classic", "pop", "classic", "classic", "pop"]	[500, 600, 150, 800, 2500]	[4, 1, 3, 0]
+# 입출력 예 설명
+# classic 장르는 1,450회 재생되었으며, classic 노래는 다음과 같습니다.
+
+# 고유 번호 3: 800회 재생
+# 고유 번호 0: 500회 재생
+# 고유 번호 2: 150회 재생
+# pop 장르는 3,100회 재생되었으며, pop 노래는 다음과 같습니다.
+
+# 고유 번호 4: 2,500회 재생
+# 고유 번호 1: 600회 재생
+# 따라서 pop 장르의 [4, 1]번 노래를 먼저, classic 장르의 [3, 0]번 노래를 그다음에 수록합니다.
+
+# ※ 공지 - 2019년 2월 28일 테스트케이스가 추가되었습니다.
+
+def solution(genres, plays):
+    gen_d = {}
+    max_d = {}
+    max_l = []
+    ans = []
+    for x,y in enumerate(genres):
+        genres[x] = [x,y]
+        gen_d[y] = []
+        max_d[y] = 0
+    
+    for genre in genres:
+        gen_d[genre[1]].append([plays[genre[0]],genre[0],])
+        gen_d[genre[1]] = sorted(gen_d[genre[1]] ,key = lambda x: (x[0], -x[1]),reverse=True)
+        max_d[genre[1]] += plays[genre[0]]
+    
+    for key in max_d.keys():
+        max_l.append([max_d[key],key])
+    max_l.sort(reverse = True)
+    
+    for val in max_l:
+        c = 0
+        for value in gen_d[val[1]]:
+            ans.append(value[1])
+            c += 1
+            if c == 2:
+                break
+    return ans
