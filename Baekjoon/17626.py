cdcf88559fcb50bceef8b4ad02b0f5936882bc00
DongGuk-Seo <failnxid@gmail.com>
2022-06-02 23:46:04 +0900

    new

--- Baekjoon/17626.py
+++ Baekjoon/17626.py
@@ -8,12 +8,12 @@ for i in range(int(n**0.5),int((n//4)**0.5),-1):
         x = n - i*i
         for o in range(int(x**0.5),int((x//3)**0.5),-1):
             if o*o == x:
-                c = 2
+                c = min(2,c)
                 break
             else:
                 y = n - i*i - o*o
                 for p in range(int(y**0.5),int((y//2)**0.5),-1):
                     if p*p == y:
-                        c = 3
+                        c = min(3,c)
                         break
 print(c)
